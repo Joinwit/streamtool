3d696cd0f63fcd25d49f5c8661346e3f20cda056
Joinwit <105548306+Joinwit@users.noreply.github.com>
2022-05-24 17:54:30 +0700

    Update playlist.php

--- app/www/playlist.php
+++ app/www/playlist.php
@@ -27,7 +27,7 @@ if (isset($_GET['e2'])) {
     foreach ($user->categories as $category) {
         foreach ($category->streams as $stream) {
             if ($stream->running == 1) {
-                echo "#SERVICE 1:0:1:0:0:0:0:0:0:0:http%3A//" . $setting->webip . "%3A" . $setting->webport . "/playlist/" . $user->username . "/" . $user->password . "/" . $stream->id . "\r\n";
+                echo "#SERVICE 1:0:1:0:0:0:0:0:0:0:http%3A//" . $setting->webip . "%3A" . $setting->webport . "/live/" . $user->username . "/" . $user->password . "/" . $stream->id . "\r\n";
                 echo "#DESCRIPTION " . $stream->name . "\r\n";
             }
         }
@@ -48,7 +48,7 @@ if (isset($_GET['m3u'])) {
                 } else {
                     echo "#EXTINF:0 tvg-logo=\"" . $setting->logourl . "" . $stream->logo . "\" tvg-id=\"" . $stream->tvid . "\" ," . $stream->name . "\r\n";
                 }
-                echo "http://" . $setting->webip . ":" . $setting->webport . "/playlist/" . $user->username . "/" . $user->password . "/" . $stream->id . "\r\n";
+                echo "http://" . $setting->webip . ":" . $setting->webport . "/live/" . $user->username . "/" . $user->password . "/" . $stream->id . "\r\n";
             }
         }
     }
@@ -66,7 +66,7 @@ if (isset($_GET['allfrtvwindows'])) {
             if ($stream->running == 1) {
                 echo "<channel>\r\n";
                 echo "<name>" . $stream->name . "</name>\r\n";
-                echo "<HQ>http://" . $setting->webip . ":" . $setting->webport . "/playlist/" . $user->username . "/" . $user->password . "/" . $stream->id . "</HQ>\r\n";
+                echo "<HQ>http://" . $setting->webip . ":" . $setting->webport . "/live/" . $user->username . "/" . $user->password . "/" . $stream->id . "</HQ>\r\n";
                 echo "<typeHQ>hls</typeHQ>\r\n";
                 echo "<recordableHQ>true</recordableHQ>\r\n";
                 echo "<category>IPTV</category>\r\n";
